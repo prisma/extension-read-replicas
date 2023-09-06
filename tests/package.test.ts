b6f1e0e63eee2123ca9214fcbd73d9edb22b2781
Sergey Tatarintsev <tatarintsev@prisma.io>
2023-09-06 13:40:51 +0200

    fix: Ensure that entries in export map point to existing files

--- tests/package.test.ts
+++ tests/package.test.ts
@@ -1,5 +1,7 @@
 import execa from 'execa'
 import path from 'path'
+import fs from 'fs/promises'
+import pkg from '../package.json'
 
 function runFile(file: string) {
   return execa('node', [path.join(__dirname, file)])
@@ -11,4 +13,22 @@ test('common js module can be loaded', async () => {
 
 test('ES module can be loaded', async () => {
   await runFile('esm.mjs')
+})
+
+async function assertFileExists(filePath: string) {
+  const stat = await fs.stat(filePath)
+  expect(stat.isFile()).toBe(true)
+}
+
+test('entires in package json point to existing files', async () => {
+  await assertFileExists(pkg.main)
+  await assertFileExists(pkg.module)
+  await assertFileExists(pkg.types)
+  for (const exportDefinition of Object.values(pkg.exports)) {
+    for (const entry of Object.values(exportDefinition)) {
+      for (const filePath of Object.values(entry)) {
+        await assertFileExists(filePath)
+      }
+    }
+  }
 })
